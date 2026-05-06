0142f641139676db9c986824dc6307f4716d7a0e
Jinjie Chen <jinjiechen25@gmail.com>
2026-05-06 19:18:25 +0000

    feat(adaptador): complete adapter methods with iterator-based list traversal

--- src/prog2/adaptador/Adaptador.java
+++ src/prog2/adaptador/Adaptador.java
@@ -6,7 +6,13 @@ import prog2.model.Prestec;
 import prog2.model.Usuari;
 import prog2.vista.BiblioException;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Adaptador {
@@ -17,40 +23,46 @@ public class Adaptador {
     }
 
     public void afegirExemplar(String id, String titol, String autor, boolean admetPrestecLlarg) throws BiblioException {
-        throw new UnsupportedOperationException("TO-DO");
+        dades.afegirExemplar(id, titol, autor, admetPrestecLlarg);
     }
 
     public List<String> recuperaExemplars() {
         ArrayList<String> result = new ArrayList<>();
-        for (Exemplar e : dades.recuperaExemplars()) {
+        Iterator<Exemplar> it = dades.recuperaExemplars().iterator();
+        while (it.hasNext()) {
+            Exemplar e = it.next();
             result.add(e.toString());
         }
         return result;
     }
 
     public void afegirUsuari(String email, String nom, String adreca, boolean esEstudiant) throws BiblioException {
-        throw new UnsupportedOperationException("TO-DO");
+        dades.afegirUsuari(email, nom, adreca, esEstudiant);
     }
 
     public List<String> recuperaUsuaris() {
         ArrayList<String> result = new ArrayList<>();
-        for (Usuari u : dades.recuperaUsuaris()) {
+        Iterator<Usuari> it = dades.recuperaUsuaris().iterator();
+        while (it.hasNext()) {
+            Usuari u = it.next();
             result.add(u.toString());
         }
         return result;
     }
 
     public void afegirPrestec(int exemplarPos, int usuariPos, boolean esLlarg) throws BiblioException {
-        throw new UnsupportedOperationException("TO-DO");
+        dades.afegirPrestec(exemplarPos, usuariPos, esLlarg);
     }
 
     public void retornarPrestec(int position) throws BiblioException {
-        throw new UnsupportedOperationException("TO-DO");
+        dades.retornarPrestec(position);
     }
 
     public List<String> recuperaPrestecs() {
         ArrayList<String> result = new ArrayList<>();
-        for (Prestec p : dades.recuperaPrestecs()) {
+        Iterator<Prestec> it = dades.recuperaPrestecs().iterator();
+        while (it.hasNext()) {
+            Prestec p = it.next();
             result.add(p.toString());
         }
         return result;
@@ -58,17 +70,28 @@ public class Adaptador {
 
     public List<String> recuperaPrestecsNoRetornats() {
         ArrayList<String> result = new ArrayList<>();
-        for (Prestec p : dades.recuperaPrestecsNoRetornats()) {
+        Iterator<Prestec> it = dades.recuperaPrestecsNoRetornats().iterator();
+        while (it.hasNext()) {
+            Prestec p = it.next();
             result.add(p.toString());
         }
         return result;
     }
 
     public void guardaDades(String camiDesti) throws BiblioException {
-        throw new UnsupportedOperationException("TO-DO");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(camiDesti))) {
+            oos.writeObject(dades);
+        } catch (IOException e) {
+            throw new BiblioException("Error en guardar dades: " + e.getMessage());
+        }
     }
 
     public void carregaDades(String camiOrigen) throws BiblioException {
-        throw new UnsupportedOperationException("TO-DO");
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(camiOrigen))) {
+            Object obj = ois.readObject();
+            this.dades = (Dades) obj;
+        } catch (IOException | ClassNotFoundException | ClassCastException e) {
+            throw new BiblioException("Error en carregar dades: " + e.getMessage());
+        }
     }
 }
